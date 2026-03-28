2a9755e8ba6a501013c6098ffabd52fe75d08e0e
xoproject0-collab <xoproject0@gmail.com>
2026-03-28 20:20:21 +0300

    Delete ocr.py

--- ocr.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-
-def extract_trade_text(image_path):
-    """Берёт текст с картинки трейда"""
-    img = Image.open(image_path)
-    text = pytesseract.image_to_string(img)
-    return text
